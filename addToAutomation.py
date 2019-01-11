6f36d9dc4c63b7a89daee55b04df3e740c7578d8
Ben Millar <bmillar@MacBook-Pro-7.local>
2019-01-10 21:14:53 -0600

    subscriber_series POST working

--- addToAutomation.py
+++ addToAutomation.py
@@ -1,7 +1,19 @@
 import requests
+import json
 
-print("Hello world!")
+print('Hello world!')
 
-r = requests.get('https://api.github.com/events')
+payload = {
+			'contactAutomation' :{
+					'contact': <CONTACT_ID>,
+					'automation': <SERIES_ID>
+				}
+			}
 
+params = {'api_key' : <API_KEY>, 'Content-Type' : 'application/x-www-form-urlencoded'}
+
+r = requests.post('https://<API_URL>.api-us1.com/api/3/contactAutomations', params=params, data=json.dumps(payload))
+
+print(payload['contactAutomation']['contact'])
+print(r.status_code)
 print(r.text)
