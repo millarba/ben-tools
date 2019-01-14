fcab270b617f1f90948149dbc1fbe5e8f921ebd2
Ben Millar <bmillar@MacBook-Pro-7.local>
2019-01-14 09:11:10 -0600

    add looping

--- addToAutomation.py
+++ addToAutomation.py
@@ -3,17 +3,23 @@ import json
 
 print('Hello world!')
 
-payload = {
-			'contactAutomation' :{
-					'contact': <CONTACT_ID>,
-					'automation': <SERIES_ID>
+#add contact list here
+contacts = ["ben", "kelsey"]
+#add seriesid
+seriesid = "2"
+
+for contact in contacts:
+	payload = {
+				'contactAutomation' :{
+						'contact': contact,
+						'automation': seriesid
+					}
 				}
-			}
+	#api key here
+	params = {'api_key' : <API_KEY>, 'Content-Type' : 'application/x-www-form-urlencoded'}
+	#account url here
+	r = requests.post('https://<API_URL>.api-us1.com/api/3/contactAutomations', params=params, data=json.dumps(payload))
 
-params = {'api_key' : <API_KEY>, 'Content-Type' : 'application/x-www-form-urlencoded'}
-
-r = requests.post('https://<API_URL>.api-us1.com/api/3/contactAutomations', params=params, data=json.dumps(payload))
-
-print(payload['contactAutomation']['contact'])
-print(r.status_code)
-print(r.text)
+	print(payload['contactAutomation']['contact'])
+	print(r.status_code)
+	print(r.text)
